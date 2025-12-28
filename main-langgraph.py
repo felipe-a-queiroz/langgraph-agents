2624b76febe75285e871aa61370db4167056e110
Felipe Queiroz <felipe.de.a.queiroz@outlook.com>
2025-12-28 17:47:21 -0300

    Aplicando o tavily e testando a busca de informações sobre o tempo

--- main-langgraph.py
+++ main-langgraph.py
@@ -17,8 +17,77 @@ os.environ["TAVILY_API_KEY"] = os.getenv("TAVILY_API_KEY")
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
 tool = TavilySearch(max_results=3)
-print(type(tool))
-print(tool.name)
 
 class AgentState(TypedDict):
     messages: Annotated[List[AnyMessage], operator.add]
+
+class Agent:
+    def __init__(self, model, tools, system=""):
+        self.system = system
+        graph = StateGraph(AgentState)
+        graph.add_node("llm", self.call_gemini)
+        graph.add_node("action", self.take_action)
+        graph.add_conditional_edges(
+            "llm",
+            self.exists_action,
+            {True: "action", False: END}
+        )
+        graph.add_edge("action", "llm")
+        graph.set_entry_point("llm")
+        self.graph = graph.compile()
+        self.tools = {t.name: t for t in tools}
+        self.model = model.bind_tools(tools)
+
+    def exists_action(self, state: AgentState):
+        result = state['messages'][-1]
+        return len(result.tool_calls) > 0
+
+    def call_gemini(self, state: AgentState):
+        messages = state['messages']
+        if self.system:
+            messages = [SystemMessage(content=self.system)] + messages
+        message = self.model.invoke(messages)
+        return {'messages': [message]}
+    
+    def take_action(self, state: AgentState):
+        tool_calls = state['messages'][-1].tool_calls
+        results = []
+        for t in tool_calls:
+            print(f"Calling: {t}")
+            if not t['name'] in self.tools:
+                print(f"\n bad tool name....")
+                result = "bad tool name, retry"
+            else:
+                result = self.tools[t['name']].invoke(t['args'])
+            results.append(ToolMessage(tool_call_id=t['id'], name=t['name'], content=str(result)))
+        print("Back to the model!")
+        return {'messages': results}
+    
+prompt = """Você é um assistente de pesquisa inteligente. Use o mecanismo de busca para procurar informações. \
+Você tem permissão para fazer múltiplas chamadas (seja em conjunto ou em sequência). \
+Procure informações apenas quando tiver certeza do que você quer. \
+Se precisar pesquisar alguma informação antes de fazer uma pergunta de acompanhamento, você tem permissão para fazer isso!
+"""
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
+
+abot = Agent(model, [tool], system=prompt)
+
+messages = [HumanMessage(content="Como estará o tempo em Recife amanhã?")]
+
+print("Iniciando a conversa...")
+final_result_state = None
+
+for s in abot.graph.stream({'messages': messages}):
+    print(s)
+    print("-----")
+    final_result_state = s
+
+print("Conversa finalizada.")
+print("Resposta final:")
+# Verifica se o estado final existe 
+if final_result_state and 'llm' in final_result_state and final_result_state['llm']['messages']:
+    # Acessa a lista de mensagens dentro de 'llm' e pega o conteúdo da última mensagem
+    print(final_result_state['llm']['messages'][-1].content[0]['text'])
+else:
+    print("Nenhuma resposta gerada pelo agente.")
